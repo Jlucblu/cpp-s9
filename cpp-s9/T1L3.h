0e62a8995f37a435e8585ca8b00fab330db1451c
Artur Smirnov <artursmirnovserg@gmail.com>
2022-05-28 20:38:35 +0300

    S9T1S4

---
+++ cpp-s9/T1L3.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class MoneyBox {
+public:
+    explicit MoneyBox(vector<int64_t> nominals)
+        : nominals_(move(nominals))
+        , counts_(nominals_.size()) {
+    }
+
+    const vector<int>& GetCounts() const {
+        return counts_;
+    }
+
+    int CoinIndex(int value) {
+        return find(nominals_.begin(), nominals_.end(), value) - nominals_.begin();
+    }
+
+    void PushCoin(int64_t value) {
+        ++counts_[CoinIndex(value)];
+    }
+
+    void PrintCoins(ostream& out) const {
+        for (int i = 0; i < counts_.size(); ++i) {
+            if (counts_[i] > 0) {
+                out << nominals_[i] << ": "s << counts_[i] << endl;
+            }
+        }
+    }
+
+private:
+    const vector<int64_t> nominals_;
+    vector<int> counts_;
+    int index = 0;
+};
+
+ostream& operator<<(ostream& out, const MoneyBox& cash) {
+    cash.PrintCoins(out);
+    return out;
+}
+
+void TestCoinIndex() {
+    MoneyBox cash({ 1, 500, 10000 });
+    cash.PushCoin(500);
+    cash.PushCoin(500);
+    cash.PushCoin(10000);
+    assert((cash.GetCounts() == vector<int>{0, 2, 1}));
+    cout << cash << endl;
+}
--- cpp-s9/main.cpp
+++ cpp-s9/main.cpp
@@ -1,56 +1,109 @@
 ﻿#include "T1L2.h"
+#include "T1L3.h"
 
-#include <cassert>
+#include <array>
+#include <iomanip>
 #include <iostream>
+#include <optional>
+#include <sstream>
+#include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 
-class MoneyBox {
+class VehiclePlate {
 public:
-    explicit MoneyBox(vector<int64_t> nominals)
-        : nominals_(move(nominals))
-        , counts_(nominals_.size()) {
+    VehiclePlate(char l0, char l1, int digits, char l2, int region)
+        : letters_{ l0, l1, l2 }
+        , digits_(digits)
+        , region_(region) {
     }
 
-    const vector<int>& GetCounts() const {
-        return counts_;
+    string ToString() const {
+        ostringstream out;
+        out << letters_[0] << letters_[1];
+        // чтобы дополнить цифровую часть номера слева нулями
+        // до трёх цифр, используем подобные манипуляторы:
+        // setfill задаёт символ для заполнения,
+        // right задаёт выравнивание по правому краю,
+        // setw задаёт минимальное желаемое количество знаков
+        out << setfill('0') << right << setw(3) << digits_;
+        out << letters_[2] << setw(2) << region_;
+
+        return out.str();
     }
 
-    int CoinIndex(int value) {
-        return find(nominals_.begin(), nominals_.end(), value) - nominals_.begin();
+    int Hash() const {
+        return digits_;
     }
 
-    void PushCoin(int64_t value) {
-        ++counts_[CoinIndex(value)];
+    bool operator==(const VehiclePlate& other) const {
+        return (digits_ == other.digits_) && (region_ == other.region_) && (letters_ == other.letters_);
     }
 
-    void PrintCoins(ostream& out) const {
-        for (int i = 0; i < counts_.size(); ++i) {
-            if (counts_[i] > 0) {
-                out << nominals_[i] << ": "s << counts_[i] << endl;
+private:
+    array<char, 3> letters_;
+    int digits_;
+    int region_;
+};
+
+ostream& operator<<(ostream& out, VehiclePlate plate) {
+    out << plate.ToString();
+    return out;
+}
+
+template <typename T>
+class HashableContainer {
+public:
+    void Insert(T elem) {
+        int index = elem.Hash();
+
+        // если вектор недостаточно велик для этого индекса,
+        // то увеличим его, выделив место с запасом
+        if (index >= int(elements_.size())) {
+            elements_.resize(index * 2 + 1);
+        }
+
+        vector<T>& hash_elem = elements_[index];
+        auto result = find(hash_elem.begin(), hash_elem.end(), elem);
+        if (result == hash_elem.end()) {
+            hash_elem.push_back(elem);
+        }
+    }
+
+    void PrintAll(ostream& out) const {
+        for (auto& e : elements_) {
+            for (auto& e2 : e) {
+                out << e2 << endl;
             }
         }
     }
 
+    const auto& GetVector() const {
+        return elements_;
+    }
+
 private:
-    const vector<int64_t> nominals_;
-    vector<int> counts_;
-    int index = 0;
+    vector<vector<T>> elements_;
 };
 
-ostream& operator<<(ostream& out, const MoneyBox& cash) {
-    cash.PrintCoins(out);
-    return out;
-}
-
 int main() {
-    //TestCountWords();
-    MoneyBox cash({ 1, 500, 10000 });
-    cash.PushCoin(500);
-    cash.PushCoin(500);
-    cash.PushCoin(10000);
-    assert((cash.GetCounts() == vector<int>{0, 2, 1}));
-    cout << cash << endl;
+    HashableContainer<VehiclePlate> plate_base;
+    plate_base.Insert({ 'B', 'H', 840, 'E', 99 });
+    plate_base.Insert({ 'O', 'K', 942, 'K', 78 });
+    plate_base.Insert({ 'O', 'K', 942, 'K', 78 });
+    plate_base.Insert({ 'O', 'K', 942, 'K', 78 });
+    plate_base.Insert({ 'O', 'K', 942, 'K', 78 });
+    plate_base.Insert({ 'H', 'E', 968, 'C', 79 });
+    plate_base.Insert({ 'T', 'A', 326, 'X', 83 });
+    plate_base.Insert({ 'H', 'H', 831, 'P', 116 });
+    plate_base.Insert({ 'P', 'M', 884, 'K', 23 });
+    plate_base.Insert({ 'O', 'C', 34, 'P', 24 });
+    plate_base.Insert({ 'M', 'Y', 831, 'M', 43 });
+    plate_base.Insert({ 'K', 'T', 478, 'P', 49 });
+    plate_base.Insert({ 'X', 'P', 850, 'A', 50 });
+
+    plate_base.PrintAll(cout);
 }
